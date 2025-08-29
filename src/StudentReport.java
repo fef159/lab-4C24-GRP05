ee9e0825e17a782d004c3685f537a3a610770dd4
moment27 <matiascohaila8@gmail.com>
2025-08-28 21:27:34 -0500

    Cambios hechos por Matias

---
+++ src/StudentReport.java
@@ -0,0 +1,8 @@
+public class StudentReport {
+    public void printReport(StudentModel student) {
+        System.out.println("===== Reporte del Estudiante =====");
+        System.out.println("Nombre: " + student.getName());
+        System.out.println("Edad: " + student.getAge() + " años");
+        System.out.println("=================================");
+    }
+}
